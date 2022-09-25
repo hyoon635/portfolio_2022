11aba65c2489dd35bb9403abc9fb6dda4df681d3
Heeyoung <hyon635@gmail.com>
2022-09-25 20:38:43 +0900

    feat(header, home): header, home component 생성 및 작업

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
