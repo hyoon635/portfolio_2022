3d7a92bb6275b922aae16263470eecadde97c45e
hyoon635 <hyoon635@gmail.com>
2022-12-13 16:20:39 +0900

    feat(header): 메뉴 추가, 메뉴 클릭시 선택한 메뉴 영역으로 자연스럽게 스크롤 되도록 작업

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  currentPage = 'home';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  goPage(page: any) {
+    this.currentPage = page;
+    document.getElementById(page)?.scrollIntoView({ behavior: 'smooth' });
   }
-
 }
