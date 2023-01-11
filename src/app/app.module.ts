2fe329ebd5aa9b26e8c360db3a0fece569a8b00e
Heeyoung <hyoon635@gmail.com>
2023-01-11 17:00:05 +0900

    feat(projects): angular swiper 플러그인 추가 및 프로젝트 리스트 swiper 형식으로 변경

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SwiperModule } from 'swiper/angular';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -10,8 +12,15 @@ import { ProjectsComponent } from './projects/projects.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, ProjectsComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, ViewChild } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required components
+import SwiperCore, { Navigation, Pagination, Autoplay, Thumbs } from 'swiper';
+import { BehaviorSubject } from 'rxjs';
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Autoplay, Thumbs]);
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
+  @ViewChild('swiperRef', { static: false }) swiperRef?: SwiperComponent;
+
   projectList = [
     {
       id: 0,
@@ -74,7 +84,94 @@ export class ProjectsComponent implements OnInit {
         'https://obsidian-patio-7a9.notion.site/cecb55ec38f1428b9d660314482b07b5',
     },
   ];
-  constructor() {}
+
+  show: boolean | undefined;
+  thumbs: any;
+  slides$ = new BehaviorSubject<string[]>(['']);
+  constructor(private cd: ChangeDetectorRef, private ngZone: NgZone) {}
 
   ngOnInit(): void {}
+
+  getSlides() {
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
+  }
+
+  thumbsSwiper: any;
+  setThumbsSwiper(swiper: any) {
+    this.thumbsSwiper = swiper;
+  }
+  controlledSwiper: any;
+  setControlledSwiper(swiper: any) {
+    this.controlledSwiper = swiper;
+  }
+
+  indexNumber = 1;
+  exampleConfig = { slidesPerView: 3 };
+  slidesPerView: number = 4;
+  pagination: any = false;
+
+  slides2 = ['slide 1', 'slide 2', 'slide 3'];
+  replaceSlides() {
+    this.slides2 = ['foo', 'bar'];
+  }
+
+  togglePagination() {
+    if (!this.pagination) {
+      this.pagination = { type: 'fraction' };
+    } else {
+      this.pagination = false;
+    }
+  }
+
+  navigation = false;
+  toggleNavigation() {
+    this.navigation = !this.navigation;
+  }
+
+  scrollbar: any = false;
+  toggleScrollbar() {
+    if (!this.scrollbar) {
+      this.scrollbar = { draggable: true };
+    } else {
+      this.scrollbar = false;
+    }
+  }
+  breakpoints = {
+    640: { slidesPerView: 2, spaceBetween: 20 },
+    768: { slidesPerView: 4, spaceBetween: 40 },
+    1024: { slidesPerView: 4, spaceBetween: 50 },
+  };
+
+  slides = Array.from({ length: 5 }).map((el, index) => `Slide ${index + 1}`);
+  virtualSlides = Array.from({ length: 600 }).map(
+    (el, index) => `Slide ${index + 1}`
+  );
+
+  log(log: string) {
+    // console.log(string);
+  }
+
+  breakPointsToggle: boolean | undefined;
+  breakpointChange() {
+    this.breakPointsToggle = !this.breakPointsToggle;
+    this.breakpoints = {
+      640: { slidesPerView: 2, spaceBetween: 20 },
+      768: { slidesPerView: 4, spaceBetween: 40 },
+      1024: { slidesPerView: this.breakPointsToggle ? 7 : 5, spaceBetween: 50 },
+    };
+  }
+
+  slidesEx = ['first', 'second'];
+
+  onSlideChange(swiper: any) {
+    if (swiper.isEnd) {
+      // all swiper events are run outside of ngzone, so use ngzone.run or detectChanges to update the view.
+      this.ngZone.run(() => {
+        this.slidesEx = [...this.slidesEx, `added ${this.slidesEx.length - 1}`];
+      });
+      console.log(this.slidesEx);
+    }
+  }
 }
